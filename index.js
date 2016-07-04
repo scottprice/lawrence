19f0af7ca95a3631454c08bd2dd1e21ad6879be3
Scott Price <scottprice1@gmail.com>
2016-07-04 00:05:15 -0400

    Added current weather support

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
-if (!process.env.SLACK_TOKEN) {
-  console.log("You need to provide a SLACK_TOKEN.");
+if (!process.env.SLACK_TOKEN || !process.env.WEATHER_TOKEN) {
+  console.log("You need to provide a SLACK_TOKEN and a WEATHER_TOKEN.");
   process.exit(1);
 }
 
 var Botkit = require('botkit');
-
+var request = require('request');
+var weatherResponse;
 var controller = Botkit.slackbot({
   debug: false
 });
@@ -17,5 +18,30 @@ var bot = controller.spawn({
 controller.hears('hello',['direct_message','direct_mention','mention', 'ambient'],function(bot,message) {
 
   bot.reply(message,'Hello yourself.');
+
+});
+
+controller.hears('weather',['direct_message','direct_mention','mention', 'ambient'],function(bot,message) {
+
+  var options = {
+    method: 'GET',
+    url: 'http://api.wunderground.com/api/f51924835a70f440/conditions/q/ga/ball_ground.json',
+    headers: {
+      'postman-token': '3ac85f00-f4af-738c-4f69-2c355c09eb38',
+      'cache-control': 'no-cache' }
+    };
+
+  request(options, function (error, response, body) {
+    if (!error) {
+      weatherResponse = JSON.parse(body);
+      console.log(weatherResponse.current_observation.observation_location.full);
+      bot.reply(message, 'Currently it is ' + weatherResponse.current_observation.weather);
+      bot.reply(message, 'The temperature is ' + weatherResponse.current_observation.temperature_string);
+      bot.reply(message, 'The location is ' + weatherResponse.current_observation.observation_location.full);
+      bot.reply(message, weatherResponse.current_observation.observation_time);
+    }
+  });
+
+
 
 });
